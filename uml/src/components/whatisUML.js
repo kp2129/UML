d3c7952ced8659fdd564478c26100ab00e1a6da7
Rennge12 <regnars.ssteins@gmail.com>
2023-11-07 13:43:01 +0200

    What is UML

---
+++ uml/src/components/whatisUML.js
@@ -0,0 +1,25 @@
+import '../App.css';
+function whatisUML() {
+    return (
+     <>
+     <div className="whatisUML">
+        <div className="title">
+            <h1>Kas ir UML un vai tā ir struktūras vai uzvedības modeļa daļa?</h1>
+        </div>
+        <div className="split">
+        <div className="text">
+            <p>UML ir vienota modelēšanas valoda, UML ir liela nozīme gan programmatūras izstrādē, gan ar programmatūru nesaistītās sistēmās 
+                daudzās nozarēs, jo tā ir veids, kā parādīt sistēmas vai procesa uzvedību un struktūru. </p>
+        </div>
+        <div className="text">
+            <p>UML modeļa daļas iedalās divās kategorijās- struktūras un uzvedības. 
+            Kopā ir 14 diagrammu tipi, kas iedalās abās šajās kategorijās.</p>
+        </div>
+        </div>
+     </div>
+     
+     </>
+    );
+  }
+
+  export default whatisUML;
