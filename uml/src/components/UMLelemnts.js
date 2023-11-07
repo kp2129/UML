050dad217db5ea7087aaab7ec7eb8fda4ba09ca4
kp2129 <kristapspolis0@gmail.com>
2023-11-07 13:16:24 +0200

    'app.js'

---
+++ uml/src/components/UMLelemnts.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function UMLelemnts() {
+  return (
+    <div>
+      <h1>Slide 1</h1>
+      <p>This is the content for Slide 1.</p>
+    </div>
+  );
+}
+
+export default UMLelemnts;
