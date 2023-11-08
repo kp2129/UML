ff35ab6dbe2b000821cfec56ab3763cb49de02ad
Rennge12 <regnars.ssteins@gmail.com>
2023-11-08 09:45:23 +0200

    Slide 4

--- uml/src/components/ExampleExplanation.js
+++ uml/src/components/ExampleExplanation.js
@@ -4,19 +4,23 @@ import '../App.css';
 function ExampleExplanation() {
   return (
     <div className='slide'>
-      <h1 className='slide-title'>Komponentu diagramma UML</h1>
-      <p className='slide-text'>
-      dsa
-      </p>
-      <img
-        src='https://cdn-images.visual-paradigm.com/guide/uml/what-is-component-diagram/02-component-diagram-overview.png'
-        alt='Komponentu diagramma'
-        className='slide-image'
-      />
-      <p className='slide-text'>
-        dsadsa
-      </p>
-    </div>
+    <h1>Komponentu diagramma UML</h1>
+    <p>Klases UML diagramma ir visizplatītākais diagrammas veids programmatūras dokumentēšanai. 
+      Tā kā lielākā daļa programmatūras, kas tiek radīta mūsdienās, joprojām ir balstīta uz objektorientētās programmēšanas paradigmu, 
+      klašu diagrammu izmantošana programmatūras dokumentēšanai ir vispareizākais risinājums. Tas notiek tāpēc, ka OOP pamatā ir klases 
+      un attiecības starp tām.</p>
+    <img
+      src='https://tallyfy.com/wp-content/uploads/2018/02/Communciation-Diagram-1024x683.png'
+      alt='Komponentu diagramma'
+      className='slide-image'
+    />
+    <p>
+    Īsāk sakot, klašu diagrammās ir ietvertas klases, kā arī to atribūti (saukti arī par datu laukiem) un uzvedība (saukta 
+    arī par dalībfunkcijām). Konkrētāk, katrai klasei ir 3 lauki: klases nosaukums augšpusē, klases atribūti tieši zem nosa
+    ukuma, klases darbības/rīcības apakšā. Attiecības starp dažādām 
+    klasēm (attēlotas ar savienojošo līniju) veido klašu diagrammu.
+    </p>
+  </div>
   );
 }
 
